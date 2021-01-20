93bcf627385863664ff3c77484c8ff22c72e9175
Logan Ward <ward.logan.t@gmail.com>
2021-01-20 16:03:18 -0600

    Add missing download script

---
+++ molecular-property-prediction/download-data.py
@@ -0,0 +1,9 @@
+"""Download the datasets needed for this work."""
+
+import pandas as pd
+import os
+
+# QM9: For this, we use the Pandas-friendly version
+# from: https://github.com/globus-labs/g4mp2-atomization-energy
+data = pd.read_json('https://github.com/globus-labs/g4mp2-atomization-energy/raw/master/data/output/g4mp2_data.json.gz', lines=True)
+data.to_json(os.path.join('datasets', 'qm9.json.gz'), orient='records', lines=True)
